0c655107beb8c9b784e039c0f6b360e1c8579c36
Daria Komarynska <01165657@pw.edu.pl>
2021-01-08 17:46:42 +0100

    Test - get list of possible moves for pawn in the beginning of the game, in the middle, basic moves for neutron, neutron doesn't have possible moves, empty place doesn't have moves.

--- test_classPawn.py
+++ test_classPawn.py
@@ -36,6 +36,59 @@ def test_present_neutron():
     pawn = Pawn(3, 2, 2)
     assert str(pawn) == "🐹"
 
+def test_get_moves_white_beginning():
+    pawn = Pawn(2, 3, 5)
+    board = Board()
+    assert pawn.get_moves(board, 3, 5) == [[3, 4], [1, 3], [5, 3]]
 
-# Test class Pawn. Basic elements, raise error if incorrect
- number, column, row
+
+def test_get_moves_neutron_middle_of_game():
+    pawn = Pawn(3, 3, 3)
+    board = Board()
+    board._board[4][3] = Pawn(2, 3, 4)
+    assert pawn.get_moves(board, 3, 3) == [
+        [1, 3],
+        [5, 3],
+        [3, 2],
+        [2, 4],
+        [4, 4],
+        [2, 2],
+        [4, 2],
+    ]
+
+
+def test_get_moves_black_middle_of_game():
+    pawn = Pawn(1, 2, 4)
+    board = Board()
+    board._board[2][1] = Pawn(2, 1, 2)
+    board._board[3][2] = Pawn(3, 2, 3)
+    board._board[5][2] = Pawn(0, 2, 5)
+    board._board[4][2] = Pawn(1, 2, 4)
+    board._board[1][5] = Pawn(2, 5, 1)
+    board._board[3][3] = Pawn(0, 3, 3)
+    board._board[5][5] = Pawn(0, 5, 5)
+    board._board[2][3] = Pawn(2, 3, 2)
+    board._board[1][3] = Pawn(0, 3, 1)
+    board._board[2][4] = Pawn(1, 4, 2)
+    board._board[5][4] = Pawn(0, 4, 5)
+    assert pawn.get_moves(board, 2, 4) == [[1, 4], [5, 4], [2, 5], [1, 3], [3, 3]]
+
+
+def test_get_moves_neutron_zero_moves():
+    pawn = Pawn(3, 3, 2)
+    board = Board()
+    board._board[2][3] = Pawn(3, 3, 2)
+    board._board[2][2] = Pawn(2, 2, 2)
+    board._board[3][2] = Pawn(2, 2, 3)
+    board._board[3][3] = Pawn(2, 3, 3)
+    board._board[3][4] = Pawn(2, 4, 3)
+    board._board[2][4] = Pawn(2, 4, 2)
+    for j in range(1, 6):
+        board._board[5][j] = Pawn(0, j, 5)
+    assert pawn.get_moves(board, 3, 2) == []
+
+
+def test_get_moves_for_empty():
+    pawn = Pawn(0, 3, 2)
+    board = Board()
+    assert pawn.get_moves(board, 3, 2) == []
