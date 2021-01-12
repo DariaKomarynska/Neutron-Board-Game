8668fef332c6fe6af776a528c1b0f047ae5edcac
Daria Komarynska <01165657@pw.edu.pl>
2021-01-12 15:50:47 +0100

    Splitted func in Game into small

--- classGame.py
+++ classGame.py
@@ -6,128 +6,8 @@ class Game:
     # def __init__(self):
     #     b = Board()
 
-    def start_rules_game(self):
-        b = Board()
-        print("The Game of Neutron\nStart new game!")
-        print()
-        print(b)
-        quit = True
-        # Start game from rules or play now
-        print(
-            "If you want to read short rules, enter '1'\
-            \nTo start game between 2 people, enter '2'\
-            \nTo start light game with computer, enter '3'\
-            \nTo start hard game with computer, enter '4'\
-            \nTo start game between hard and light computers, enter '5'"
-        )
-        while quit:
-            # Check: is input number correct?
-            start = input("Your choice: ")
-            print()
-            if len(start) != 1:
-                print("Enter 1, 2, 3, 4 or 5 without space.")
-                quit = True
-            else:
-                quit = False
-
-            if quit is False:
-                # Check: is input element digit?
-                if not start.isdigit():
-                    print("Please, write correctly.")
-                    quit = True
-                elif start.isdigit():
-                    n_start = int(start)
-                    quit = False
-
-            if quit is False:
-                # Check: is input element 1-5?
-                if n_start not in range(1, 6):
-                    print("Please, choose 1, 2, 3, 4 or 5")
-                    quit = True
-
-            flag = True
-
-        while quit is False:
-            # 1 if player want to rules
-            if n_start == 1 and flag is True:
-                print(
-                    "RULES\
-                \nWinning the game is simple.\
-                \nJust maneuver the neutron onto your own back row."
-                )
-                print()
-                print(
-                    "Every",
-                    "piece, even the neutron, moves in excactly the",
-                    "same way:\nalong a straight line horizontally,",
-                    "vertically, or diagonally.\nA piece stops moving",
-                    "just before it runs into another piece or the side",
-                    "wall.\nThere is no jumping or capturing. But there",
-                    "is one unusual feature.\nWhen a piece moves, it must",
-                    " go as far as possible in the direction chosen.",
-                )
-                print()
-                flag = False
-            if n_start == 1 and flag is False:
-                print()
-                print(
-                    "If you want to start game between two people, enter '2'",
-                    "\nIf you want to start light game with computer,",
-                    "enter '3'\
-                    \nIf you want to start hard game with computer,",
-                    "enter '4'",
-                    "\nIf you want to start game between hard and light computers,",
-                    "enter '5'",
-                    "\nIf you don't want to play, enter '0'",
-                )
-                new_start = input("Your choice: ")
-
-                # Check: is input element correct?
-                if len(new_start) != 1:
-                    print("Try again.")
-                    quit = False
-                    flag = False
-                else:
-                    quit = True
-
-                if quit is True:
-
-                    if not new_start.isdigit():
-                        # Check: is input element digit?
-                        print("Please, write correctly.")
-                        quit = False
-                        flag = False
-                    elif new_start.isdigit():
-                        new_start = int(new_start)
-                        quit = True
-                if quit is True:
-                    if new_start == 0:
-                        # 0 if player don't want to play
-                        print("END")
-                        break
-
-                    elif new_start == 2 or new_start == 3 or new_start == 4 or new_start == 5:
-                        # 2 if start the game between two people
-                        # 3 if start the game between human and light computer
-                        # 4 if start the game between human and hard computer
-                        self.start_game(new_start)
-                        break
-
-                    else:
-                        # if input number is not 2, 3, 4, 5 or 0
-                        print("Enter only 2, 3, 4, 5 or 0.")
-                        quit = False
-                        flag = False
-
-            elif n_start == 2 or n_start == 3 or n_start == 4 or n_start == 5:
-                self.start_game(n_start)
-                break
-
-    def game_two_people_2_3(self, version, black_won, white_won):
+    def game_two_people_light_2_3(self, version, white, black, neutron, black_won, white_won):
         counter = 23
-        white = Figure.WHITE
-        black = Figure.BLACK
-        neutron = Figure.NEUTRON
         b = Board()
         print("Player-1 - 🟡 | Player-2 - 🟣")
         print(b)
@@ -173,11 +53,8 @@ class Game:
                 break
         print()
 
-    def game_with_hard_computer_4_5(self, version, black_won, white_won):
+    def game_with_hard_computer_4_5(self, version, white, black, neutron, black_won, white_won):
         counter = 45
-        white = Figure.WHITE
-        black = Figure.BLACK
-        neutron = Figure.NEUTRON
         b = Board()
         print("Player-1 - 🟣 | Player-2 - 🟡")
         self.print_b(b)
@@ -227,7 +104,7 @@ class Game:
         print()
         print(b)
 
-    def start_game(self, version):
+    def play_all_versions(self, version):
         white_won = "Player-2 has fallen into a trap and cannot move Neutron\
                     \n🟣 Player-2 🟣 is Looser\
                     \n🟡 Player-1 🟡 is WINNER"
@@ -248,7 +125,89 @@ class Game:
         print("Write X and Y of pawn: two numbers from 1 to 5 with", "space")
         print()
         if version == 2 or version == 3:
-            self.game_two_people_2_3(version, black_won, white_won)
+            self.game_two_people_light_2_3(version, white, black, neutron, black_won, white_won)
         else:
-            self.game_with_hard_computer_4_5(version, black_won_hard, white_won_hard)
+            self.game_with_hard_computer_4_5(version, white, black, neutron, black_won_hard, white_won_hard)
+
+    def start_choice_print(self, board, place):
+        # Start game from rules or play now
+        if place == 1:
+            print("The Game of Neutron\nStart new game!")
+            self.print_b(board)
+        print()
+        if place == 1:
+            print("If you want to read short rules, enter '1'")
+        print(
+            "To start game between 2 people, enter '2'\
+            \nTo start light game with computer, enter '3'\
+            \nTo start hard game with computer, enter '4'\
+            \nTo start game between hard and light computers, enter '5'"
+        )
+        if place == 0:
+            print("If you don't want to play, enter '0'")
+
+    def rules_print(self):
+        print(
+            "RULES\
+        \nWinning the game is simple.\
+        \nJust maneuver the neutron onto your own back row."
+        )
+        print()
+        print(
+            "Every",
+            "piece, even the neutron, moves in excactly the",
+            "same way:\nalong a straight line horizontally,",
+            "vertically, or diagonally.\nA piece stops moving",
+            "just before it runs into another piece or the side",
+            "wall.\nThere is no jumping or capturing. But there",
+            "is one unusual feature.\nWhen a piece moves, it must",
+            " go as far as possible in the direction chosen.",
+        )
+        print()
+
+    def choose_correctly(self, place):
+        quit = True
+        while quit:
+            # Check: is input number correct?
+            start = input("Your choice: ")
+            print()
+            if (
+                len(start) == 1
+                and start.isdigit()
+            ):
+                # Check number of input elements
+                # Check: is input element digit?
+                start = int(start)
+                if ((start not in range(1, 6) and place == 1)
+                    or ((start not in range(2, 6) and start != 0) and place == 0)):
+                    print("Try again")
+
+                else:
+                    quit = False
+                    break
+            else:
+                print("Try again")
+        return start
+
+    def start_rules_game(self):
+        b = Board()
+        self.start_choice_print(b, 1)
+        start = self.choose_correctly(1)
+        if start == 1:
+            self.rules_print()
+            self.start_choice_print(b, 0)
+            start = self.choose_correctly(0)
+        if start == 2 or start == 3 or start == 4 or start == 5:
+            # 2 if start the game between two people
+            # 3 if start the game between human and light computer
+            # 4 if start the game between human and hard computer
+            # 5 if start the game between hard and light computers
+            self.play_all_versions(start)
+        elif start == 0:
+            print("END")
+
+
+
+
+
 
